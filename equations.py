91ab4694f7c7703f062ef1327d05407408a0b0ba
Kay Altos <kayaltosnexus@gmail.com>
2024-04-04 20:01:24 +0300

    Добавил серию графиков для плотность-давление при разном G

--- equations.py
+++ equations.py
@@ -23,9 +23,16 @@ def vdw_pressure (V, n, T, a, b, R=0.0821):
         return (n*R*T)/(V-n*b) - a*(n/V)*(n/V)
     except:
         print(f'Деление на ноль: V={V}, b={b}')
-
 def vdw_pressure_molar (Vm, T, a, b, R=0.0821):
     try:
         return (R*T)/(Vm-b) - a*(1/Vm)*(1/Vm)
     except:
         print(f'Деление на ноль: V={Vm}, b={b}')
+
+# Psi function for One-Component Shan Chen
+def psi (rho, psi0 = 4, rho0 = 200):
+    return psi0*math.exp(-rho0/rho)
+
+# Shan Chen equation of state
+def shan_chen_pressure (G, rho=200):
+    return rho/3 + (G/6)*psi(rho)*psi(rho)
--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import equations
 x = np.arange(0.05, 0.5, 0.001)
 y = np.vectorize(equations.vdw_pressure)
 
-for T in range(200, 500, 20):
-    plt.plot(x, y(x, n=1, T=T, a=3.59, b=0.04267))
+for T in range(210, 500, 20):
+    plt.plot(x, y(x, n=1, T=T, a=3.59, b=0.04267), label=f'T={T}')
+plt.legend()
+plt.show()
+
+# Кривые rho-V для Шань-Ченя
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+density = np.arange(1, 1000, 0.01)
+pressure = np.vectorize(equations.shan_chen_pressure)
+for G in range (-144, -47, 12):
+    #plt.text(1000, 200 - 0.65*abs(G), f'G={G}', font)
+    plt.plot(density, pressure(G, density), label=f'G={G}')
+plt.legend()
 plt.show()
